0ae124c3b8ac402147eba9ad09274cca07dfeffe
Vandalious <arash.hajiannezhad@gmail.com>
2023-01-13 13:07:14 +0330

    Add competition notebooks.

---
+++ round 1 (warmup)/1. Nazarie Risman.py
@@ -0,0 +1,16 @@
+"""
+Codecup Competition Series 7: Data Science | Part 1
+
+Author: Arash Hajian nezhad
+"""
+
+
+# read two lines
+word = input()
+sentence = input()
+
+# check
+if word in sentence:
+    print(1)
+else:
+    print(0)
